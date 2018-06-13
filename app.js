7b8ea82c5fed235e4ee3e7a86690275c0a17eed1
İSA TOLTAR <38230661+toltarisa@users.noreply.github.com>
2018-06-13 16:24:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ document.getElementById('InchInput').addEventListener('input',function(e){
     document.getElementById('output').style.visibility="visible";
     var Inch = e.target.value;
     document.getElementById('cmOutput').innerHTML=Inch*2.54;
-    document.getElementById('ftOutput').innerHTML=Inch/0.083;
-    document.getElementById('ydOutput').innerHTML=Inch/0.027;
+    document.getElementById('ftOutput').innerHTML=Inch*0.083;
+    document.getElementById('ydOutput').innerHTML=Inch*0.027;
 });
